bff9b7162de3f3e2e2012466c9b986c81d7f4414
padminijoshi <joshipj2000@gmail.com>
2023-11-29 00:16:40 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Route, 
-  Routes,
-  BrowserRouter
-} from "react-router-dom";
+//import {
+ // BrowserRouter as Router,
+ // Route, 
+ // Routes,
+ // BrowserRouter
+//} from "react-router-dom";
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
@@ -53,17 +53,17 @@ function App() {
   return (
     <>
       {/*<Navbar title="TextUtils" aboutText="About TextUtils"/>*/}
-      <BrowserRouter>
+      {/*<BrowserRouter>*/}
       <Navbar title="TextUtils" mode={mode} changeredColor={changeredColor} changeyellowColor={changeyellowColor} changegreenColor={changegreenColor} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my-3">
-      <Routes> 
-      <Route exact path="/about" element={<About />}></Route>
-          <Route exact path="/" element={ <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}>
-          </Route>
-      </Routes>
+     {/* <Routes> */} 
+      {/*<Route exact path="/about" element={<About />}></Route>*/}
+      <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+      {/*    </Route>*/}
+      {/*</Routes>*/}
         </div>
-      </BrowserRouter>
+      {/*</BrowserRouter>*/}
       
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import { Link } from "react-router-dom";
+
 
 
 export default function Navbar(props) {
@@ -8,7 +8,7 @@ export default function Navbar(props) {
     
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="#">
             {props.title}
           </a>
           <button
@@ -24,16 +24,19 @@ export default function Navbar(props) {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+              <li className="nav-item">   
+              {/*<Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </Link>
+                </Link>*/} 
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
               </li>
-              <li className="nav-item">
+              {/*<li className="nav-item">
                 <Link className="nav-link" to="/about">
                   {props.aboutText}
                 </Link>
-              </li>
+              </li>*/}
             </ul>
             {/*<form className="d-flex" role="search">
               <input
@@ -64,5 +67,5 @@ Navbar.propTypes = {
 
 Navbar.defaultProps = {
   title: "Set title here",
-  aboutText: "About text here",
+  aboutText: "About",
 }
